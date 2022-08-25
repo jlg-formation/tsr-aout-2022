189b556a7c3c9a6c27f1bc372d798aaace0fe1e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-08-25 14:47:32 +0200

    dev: added points on circle

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 "use strict";
 
-const a = {
-  toto: 123,
+const samples = 10;
+
+const getAngleFromIndex = (index, samples) => {
+  return (index * 2 * Math.PI) / samples;
 };
 
-console.log("a: ", a);
+const x0 = 50;
+const y0 = 50;
+const r0 = 45;
+
+const getPositionFromAngle = (angle) => {
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
+  return { x, y };
+};
+
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("svg g.samples");
+
+for (let i = 0; i < samples; i++) {
+  const angle = getAngleFromIndex(i, samples);
+
+  const { x, y } = getPositionFromAngle(angle);
+
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x);
+  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "r", 1);
+  container.appendChild(circle);
+}

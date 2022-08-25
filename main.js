45029bf3ea477d8154bffd4303ce35814e3d0217
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-08-25 12:08:27 +0200

    dev: play with var and this

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("hello");
+"use strict";
+
+console.log("this: ", this);
+
+function toto(a, b) {
+  console.log("this: ", this);
+  console.log("a: ", a);
+  console.log("b: ", b);
+  {
+    let c = 45;
+    console.log("c: ", c);
+  }
+}
+
+toto(45, 67, 453);
+
+const a = {
+  titi: toto,
+};
+console.log("a: ", a);
+a.titi();
+
+toto.bind(123)(2, 3);
+toto.call(124, 2, 3);
+toto.apply(125, [2, 45]);
+
+const b = new toto(5, 6);
+console.log("b: ", b);
+
+const truc = (x) => {
+  console.log("this: ", this);
+};
+
+truc();

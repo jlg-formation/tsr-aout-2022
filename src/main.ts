66a064bfdd6e56441f3a502a17cda16d71e50599
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-08-25 15:35:47 +0200

    dev: transform project to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-"use strict";
-
 const samples = 10;
 
-const getAngleFromIndex = (index, samples) => {
+const getAngleFromIndex = (index: number, samples) => {
   return (index * 2 * Math.PI) / samples;
 };
 
 const x0 = 50;
 const y0 = 50;
 const r0 = 45;
+const r = 1;
 
 const getPositionFromAngle = (angle) => {
   const x = x0 + r0 * Math.cos(angle);
@@ -18,6 +17,9 @@ const getPositionFromAngle = (angle) => {
 
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("svg g.samples");
+if (container === null) {
+  throw new Error("Cannot find selector: svg g.samples");
+}
 
 for (let i = 0; i < samples; i++) {
   const angle = getAngleFromIndex(i, samples);
@@ -25,8 +27,8 @@ for (let i = 0; i < samples; i++) {
   const { x, y } = getPositionFromAngle(angle);
 
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", x);
-  circle.setAttributeNS(null, "cy", y);
-  circle.setAttributeNS(null, "r", 1);
+  circle.setAttributeNS(null, "cx", x + "");
+  circle.setAttributeNS(null, "cy", String(y));
+  circle.setAttributeNS(null, "r", `${r}`);
   container.appendChild(circle);
 }
